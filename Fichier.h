db9c9df23369154cbba744bad36bdd4ba01841f5
clemdmtle <clemence.dumontlespinats@viacesi.fr>
2024-12-06 15:07:24 +0100

    Update Fichier.h

--- Fichier.h
+++ Fichier.h
@@ -1,18 +1,12 @@
-#pragma once
-#include <string>
-using namespace std;
+#include <string> 
 
-class Fichier
-{
-private:
+class Fichier {
     string path;
 
-public:
-    Fichier(string path) {}
+    public:
+    Fichier(string path){}
 
-    void stockerDonnees(vector <Cellule*> vect);
-    void creerFichier();
-    void lireFichier();
-    auto initGrille(int nbLignes, int nbColonnes);
-    void initCellule();
+    void stockerDonnees(vector<vector<Cellule*>> vect);
+    string to_char(Cellule* c2);
+    int* lireFichier();
 };
