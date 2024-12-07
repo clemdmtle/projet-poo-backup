1257c472980b522bb69594a213a81b32aee01b23
clemdmtle <clemence.dumontlespinats@viacesi.fr>
2024-12-07 18:52:47 +0100

    lol

--- Interface.cpp
+++ Interface.cpp
@@ -1,1 +1,8 @@
 #include "Interface.h"
+#include <iostream>
+using namespace std;
+
+
+void affichageGrille(Grille *g, Fichier* f){
+    cout << "Affichage de la grille" << endl; 
+}
--- Interface.h
+++ Interface.h
@@ -6,6 +6,6 @@ class Interface
 {
 public:
     Interface();
-    virtual void affichageGrille(Grille *g, Fichier* f)=0;
+    virtual void affichageGrille(Grille *g, Fichier* f);
     ~Interface();
 };
