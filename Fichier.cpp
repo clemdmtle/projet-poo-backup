7e1dac67a36e166411d513dde1ac679031560afa
clemdmtle <clemence.dumontlespinats@viacesi.fr>
2024-12-08 04:38:50 +0100

    modif fichier

--- Fichier.cpp
+++ Fichier.cpp
@@ -16,29 +16,52 @@ Fichier::Fichier(string path)
 }
 
 vector<int> Fichier::lireFichier()
-{ // permet d'extraire nbcolonnes et nblignes
-    vector<int> tab(2);
-    ifstream fichier(path.c_str(), ios::in); // ouvre le fichier
+{ // Permet d'extraire nbcolonnes et nblignes
+    vector<int> tab(2); // Initialise un vecteur avec deux éléments
+    ifstream fichier(path.c_str(), ios::in); // Ouvre le fichier
     string strChiffre;
 
     if (fichier)
     {
         cout << "Fichier ouvert avec succès" << endl;
+
         for (int x = 0; x < 2; x++)
-        { // stocke dans un tableau nbligne et nbcolonne
-            getline(fichier, strChiffre, ' ');
-            tab[x] = stoi(strChiffre); // change le string en int
+        {
+            if (getline(fichier, strChiffre, ' '))
+            {
+                try
+                {
+                    tab[x] = stoi(strChiffre); // Convertit la chaîne en entier
+                }
+                catch (const invalid_argument &e)
+                {
+                    cerr << "Erreur : données invalides dans le fichier." << endl;
+                    return {}; // Retourne un vecteur vide
+                }
+                catch (const out_of_range &e)
+                {
+                    cerr << "Erreur : valeur hors plage dans le fichier." << endl;
+                    return {}; // Retourne un vecteur vide
+                }
+            }
+            else
+            {
+                cerr << "Erreur : données insuffisantes dans le fichier." << endl;
+                return {}; // Retourne un vecteur vide
+            }
         }
 
-        fichier.close(); // ferme le fichier
+        fichier.close(); // Ferme le fichier
     }
     else
     {
         cerr << "Erreur à l'ouverture du fichier" << endl;
+        return {}; // Retourne un vecteur vide
     }
 
     return tab;
 }
+
 
 void Fichier::stockerDonnees(vector<vector<Cellule *>> vect)
 { // permet de stocker une grille dans un fichier
